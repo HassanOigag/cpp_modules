eda52fc2bba772d5eb3758097eac062d708dfd0f
99Hassan20 <hassanoigag0@gmail.com>
2024-01-13 13:43:02 +0100

    done

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,14 +6,15 @@
 /*   By: hoigag <hoigag@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 15:31:44 by hoigag            #+#    #+#             */
-/*   Updated: 2024/01/12 19:10:05 by hoigag           ###   ########.fr       */
+/*   Updated: 2024/01/13 13:41:44 by hoigag           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "PmergeMe.hpp"
-
-
+#include <ctime>
+#include <iomanip>
+#include <time.h>
 int main(int argc, char __unused **argv)
 {
 	if (argc == 1)
@@ -24,14 +25,20 @@ int main(int argc, char __unused **argv)
 
 	try
 	{
+		double start, end;
 		std::string input = concat(argv + 1);
 		PmergeMe pmm(input);
+		start = static_cast<double>(clock()) / CLOCKS_PER_SEC;
 		pmm.parseString();
 		// pmm.printContainer(pmm.getDeque());
-		pmm.forwardRecursionDeque();
+		pmm.forwardRecursionVector();
+		end = static_cast<double>(clock()) / CLOCKS_PER_SEC;
 		// pmm.printContainer(pmm.getNumbers());
+		std::cout << start  << " " <<  end << std::endl;
+		double time = end - start;
 		std::cout << "len : " << pmm.getDeque().size() << std::endl;
 		std::cout << "comparaisons: " << i << std::endl;
+		std::cout << std::fixed << std::setprecision(15)<< "time elapsed : " << time << std::endl;
 		// pmm.printContainer(pmm.getNumbers());
 		// pmm.printNumbers();
 	}
